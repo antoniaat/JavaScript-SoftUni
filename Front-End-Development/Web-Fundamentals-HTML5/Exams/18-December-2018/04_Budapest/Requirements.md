f06928926de68d1abb02447caef4d1938677ad06
antoniyaat <antoniyaata@gmail.com>
2018-12-27 14:35:58 +0200

    Web Fundamentals - 18 december - Author solutions

---
+++ Front-End-Development/Web-Fundamentals-HTML5/Exams/18-December-2018/04_Budapest/Requirements.md
@@ -0,0 +1,13 @@
+# 04. Budapest
+
+## Tasks
+* Create a web page **(HTML5 + CSS3)** that looks and behaves like the provided screenshot
+* You are given all of the **required resources**
+* The output should be valid **HTML + CSS + images** that implement the above web page
+
+## Constraints
+* Use the font - **"Helvetica"**
+* Use **Font Awesome** for the icons
+* The site should open correctly in the latest version of Chrome
+* Pixel-perfect implementation is **NOT** required
+* You are **NOT** permitted to use libraries like Bootstrap
--- JS-Core/JS_Apps/04_Asynchronous-Programming/Exercise/01_Forecaster/scripts/forecaster.js
+++ JS-Core/JS_Apps/04_Asynchronous-Programming/Exercise/01_Forecaster/scripts/forecaster.js
@@ -1,48 +1,7 @@
 function attachEvents() {
     $('#submit').click(getWeather);
 
-
-    function getWeather() {
-        let location = $('#location').val();
-        // $('#location').val(''); // Clear the input field
-
-        let url = "https://judgetests.firebaseio.com/locations.json";
-        $.ajax({
-            url: url,
-            type: 'GET',
-            success: findLocation,
-            error: displayError
-        });
-
-        function findLocation(response) {
-            let successfulReq = false;
-
-            for (let key in response) {
-                let current = response[key];
-
-                if (current.name === location) {
-                    let locationCode = current.code;
-                    successfulReq = true;
-
-                    sendLocationCode(locationCode);
-                    viewUpcomingForecast(locationCode);
-                }
-            }
-
-            if (!successfulReq) {
-                displayError()
-            }
-        }
-
-        function sendLocationCode(locationCode) {
-            $.ajax({
-                url: `https://judgetests.firebaseio.com/forecast/today/${locationCode}.json`,
-                type: 'GET',
-                success: showTheCurrentWeather,
-                error: displayError
-            });
-        }
-
+    function appendContainers() {
         $('#current')
             .append(`<span class="condition symbol"></span>`)
 
@@ -53,74 +12,130 @@ function attachEvents() {
         condition.append(`<span class="forecast-data"></span>`);
 
         $('#current').append(condition);
+    }
 
-        function showTheCurrentWeather(res) {
-            $('#forecast')
-                .css('display', 'block');
+    appendContainers();
 
-            let symbol = attachWeatherSymbol(res.forecast.condition);
-            $('#current .condition.symbol').text(`${symbol}`);
+    function getWeather() {
+        let url = "https://judgetests.firebaseio.com/locations.json";
+        $.ajax({
+            url: url,
+            type: 'GET',
+            success: findLocation,
+            error: displayError
+        });
+    }
 
-            $(`.forecast-data:nth-child(1)`).text(`${res.name}`);
-            $(`.forecast-data:nth-child(2)`).text(`${res.forecast.low}°/${res.forecast.high}°`);
-            $(`.forecast-data:nth-child(3)`).text(`${res.forecast.condition}`);
-        }
+    function findLocation(response) {
+        let location = $('#location').val();
+        // $('#location').val(''); // Clear the input field
+        let successfulReq = false;
 
-        function attachWeatherSymbol(currentWeather) {
-            let symbol = '';
+        for (let key in response) {
+            let current = response[key];
 
-            if (currentWeather === 'Sunny') {
-                symbol = '&#x2600';
-            } else if (currentWeather === 'Partly Sunny') {
-                symbol = '&#x26C5';
-            } else if (currentWeather === 'Overcast') {
-                symbol = '&#x2601';
-            } else if (currentWeather === 'Rain') {
-                symbol = '&#x2614';
-            } else if (currentWeather === 'Degrees') {
-                symbol = '&#176';
-            }
+            if (current.name === location) {
+                let locationCode = current.code;
+                successfulReq = true;
 
-            return symbol;
-        }
-
-        function viewUpcomingForecast(locationCode) {
-            $.ajax({
-                url: `https://judgetests.firebaseio.com/forecast/upcoming/${locationCode}.json`,
-                type: 'GET',
-                success: printUpcomingForecast,
-                error: displayError
-            });
-        }
-
-        function printUpcomingForecast(response) {
-            for (let key in response) {
-                let current = response[key];
-
-                addCurrentUpcoming(current[0]);
-                addCurrentUpcoming(current[1]);
-                addCurrentUpcoming(current[2]);
-
-                break;
+                sendLocationCode(locationCode);
+                viewUpcomingForecast(locationCode);
             }
         }
 
-        function addCurrentUpcoming(current) {
-            let currentDay = $(`<span class="upcoming"></span>`);
-            let symbol = attachWeatherSymbol(current.condition);
+        if (!successfulReq) {
+            displayError();
+        }
+    }
 
-            currentDay.append(`<span class="symbol">${symbol}</span>`)
-            currentDay.append(`<span class="forecast-data">${current.low}°/${current.high}°</span>`)
-            currentDay.append(`<span class="symbol">${current.condition}</span>`)
+    function sendLocationCode(locationCode) {
+        $.ajax({
+            url: `https://judgetests.firebaseio.com/forecast/today/${locationCode}.json`,
+            type: 'GET',
+            success: showTheCurrentWeather,
+            error: displayError
+        });
+    }
 
-            $('#upcoming').append(currentDay);
+    function showTheCurrentWeather(res) {
+        $('#forecast')
+            .css('display', 'block');
+
+        let symbol = attachWeatherSymbol(res.forecast.condition);
+        $('#current .condition.symbol').text(symbol);
+
+        $(`.forecast-data:nth-child(1)`).text(`${res.name}`);
+        $(`.forecast-data:nth-child(2)`).text(`${res.forecast.low}°/${res.forecast.high}°`);
+        $(`.forecast-data:nth-child(3)`).text(`${res.forecast.condition}`);
+    }
+
+    function attachWeatherSymbol(currentWeather) {
+        let symbol = '';
+
+        if (currentWeather === 'Sunny') {
+            symbol = '☀';
+        } else if (currentWeather === 'Partly Sunny') {
+            symbol = '⛅';
+        } else if (currentWeather === 'Overcast') {
+            symbol = '☁';
+        } else if (currentWeather === 'Rain') {
+            symbol = '☂';
+        } else if (currentWeather === 'Degrees') {
+            symbol = '&#176';
         }
 
-        function displayError() {
-            $('#forecast')
-                .css('display', 'block')
-                .text('Error');
-            $('#location').val('');
+        return symbol;
+    }
+
+    function viewUpcomingForecast(locationCode) {
+        $.ajax({
+            url: `https://judgetests.firebaseio.com/forecast/upcoming/${locationCode}.json`,
+            type: 'GET',
+            success: printUpcomingForecast,
+            error: displayError
+        });
+    }
+
+    function printUpcomingForecast(response) {
+        for (let key in response) {
+            let current = response[key];
+
+            addCurrentUpcoming(current[0]);
+            addCurrentUpcoming(current[1]);
+            addCurrentUpcoming(current[2]);
+
+            break;
         }
+    }
+
+    function attachUpcomingContainers() {
+        let currentDay = $(`<span class="upcoming"></span>`);
+        let symbol = attachWeatherSymbol(current.condition);
+
+        currentDay.append(`<span class="symbol">${symbol}</span>`);
+        currentDay.append(`<span class="forecast-data">${current.low}°/${current.high}°</span>`);
+        currentDay.append(`<span class="symbol">${current.condition}</span>`);
+
+        $('#upcoming').append(currentDay);
+    }
+
+    attachUpcomingContainers();
+
+    function addCurrentUpcoming(current) {
+        let currentDay = $(`<span class="upcoming"></span>`);
+        let symbol = attachWeatherSymbol(current.condition);
+
+        currentDay.append(`<span class="symbol">${symbol}</span>`);
+        currentDay.append(`<span class="forecast-data">${current.low}°/${current.high}°</span>`);
+        currentDay.append(`<span class="symbol">${current.condition}</span>`);
+
+        $('#upcoming').append(currentDay);
+    }
+
+    function displayError() {
+        $('#forecast')
+            .css('display', 'block')
+            .text('Error');
+        $('#location').val('');
     }
 }
