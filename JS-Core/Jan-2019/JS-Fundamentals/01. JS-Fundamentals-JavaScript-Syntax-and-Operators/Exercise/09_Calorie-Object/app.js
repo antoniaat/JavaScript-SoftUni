dadbfc6f51c2502a872b8ea9ef1bb32fd47ec923
antoniyaat <antoniyaata@gmail.com>
2019-01-16 15:59:28 +0200

    Finish all exercises for JS Syntax and Operators

--- JS-Core/Jan-2019/JS-Fundamentals/01. JS-Fundamentals-JavaScript-Syntax-and-Operators/Exercise/09_Calorie-Object/app.js
+++ JS-Core/Jan-2019/JS-Fundamentals/01. JS-Fundamentals-JavaScript-Syntax-and-Operators/Exercise/09_Calorie-Object/app.js
@@ -1,9 +1,9 @@
 function solve(arr) {
     let result = {};
 
-    for (let i = 0; i < arr.length; i + 2) {
+    for (let i = 0; i < arr.length; i+=2) {
         let propertyName = arr[i];
-        let propertyValue = arr[i + 1];
+        let propertyValue = +arr[i + 1];
 
         result[propertyName] = propertyValue;
     }
---
+++ JS-Core/Jan-2019/JS-Fundamentals/01. JS-Fundamentals-JavaScript-Syntax-and-Operators/Exercise/10_Coffee-Machine/app.js
@@ -0,0 +1,67 @@
+function solve(arr) {
+    let price = 0;
+    let totalIncome = 0;
+
+    for (const line of arr) {
+        let tokens = line.split(', ');
+        let insertedCoins = +tokens[0];
+
+        let typeOfTheDrink = tokens[1]; // Coffee or tea
+
+        let supplement = tokens[2];
+        price = calculatePrice(typeOfTheDrink, supplement);
+
+        // Check for milk
+        if (tokens.includes('milk')) {
+            let milkPrice = Number(((10 * price) / 100).toFixed(1));
+            price += milkPrice;
+        }
+
+        let lastIndex = tokens.length - 1;
+        let quantityOfSugar = +tokens[lastIndex];
+        let sugarPrice = 0;
+
+        if (quantityOfSugar > 0 && quantityOfSugar <= 5) {
+            sugarPrice = 0.1;
+        }
+
+        price += sugarPrice;
+
+        printTheResult(insertedCoins, price, typeOfTheDrink);
+    }
+
+    console.log(`Income Report: ${totalIncome.toFixed(2)}$`);
+
+    function printTheResult(insertedCoins, price, drink) {
+        if (insertedCoins >= price) {
+            let change = insertedCoins - price;
+            totalIncome += price;
+            console.log(`You ordered ${drink}. Price: ${price.toFixed(2)}$ Change: ${change.toFixed(2)}$`);
+        } else {
+            let moneyNeeded = price - insertedCoins;
+            console.log(`Not enough money for ${drink}. Need ${moneyNeeded.toFixed(2)}$ more.`);
+        }
+    }
+
+    function calculatePrice(typeOfTheDrink, supplement) {
+        if (typeOfTheDrink === 'coffee') {
+            if (supplement === 'caffeine') {
+                price = 0.8;
+            } else if (supplement === 'decaf') {
+                price = 0.9;
+            }
+        } else if (typeOfTheDrink === 'tea') {
+            price = 0.8;
+        }
+
+        return price;
+    }
+}
+
+let input0 = ['1.00, coffee, caffeine, milk, 4', '0.40, tea, milk, 2',
+    '1.00, coffee, decaf, 0'];
+
+let input1 = ['8.00, coffee, decaf, 4',
+    '1.00, tea, 2'];
+
+solve(input0);
