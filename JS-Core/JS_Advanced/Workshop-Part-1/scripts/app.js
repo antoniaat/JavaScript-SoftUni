3fa33d9e2fa4c15320938480d4207ee7257119e7
Antonia <antoniyaata@gmail.com>
2019-03-08 01:25:36 +0200

    Add one more plugin to the workshop - jquery Sakura

--- JS-Core/JS_Advanced/Workshop-Part-1/scripts/app.js
+++ JS-Core/JS_Advanced/Workshop-Part-1/scripts/app.js
@@ -5,6 +5,45 @@ $(".regular").slick({
     slidesToScroll: 4
 });
 
-function addMeetingWithLecturer() {
+(function attachEvents() {
 
-}
+    $('#bookHourBtn').click(addMeetingWithLecturer);
+
+    function addMeetingWithLecturer(event) {
+        event.stopPropagation();
+        event.preventDefault();
+
+        let consultationInfo = takeTheCurrentConsultationInfo();
+        if (consultationInfo) appendTheCurrentConsultation(consultationInfo);
+    }
+
+    function updateSeminarsCount() {
+        let currentCount = $('.education article:nth-child(3) .box-body ul li').length;
+        $('.box-footer span').text(currentCount);
+
+        console.log(currentCount);
+    }
+
+    function appendTheCurrentConsultation(consultationInfo) {
+        let newConsultation = $(`<li>${consultationInfo}<i class="fas fa-chevron-circle-right"></i></li>`);
+        $('.education article:nth-child(3) .box-body ul').append(newConsultation);
+        updateSeminarsCount();
+    }
+
+    function takeTheCurrentConsultationInfo() {
+        // We can add username validation. If username is valid we can add the meeting
+        let lecturerName = $('#lecturer option:selected').text().split(' ')[0];
+
+        if (lecturerName !== 'Име') {
+            let dateAndTime = $('#dateTimeInput').val().split(' ');
+            let date = dateAndTime[0].split('/');
+            let time = dateAndTime[1];
+
+            let monthAndDay = `${date[1]}/${date[2]}`;
+
+            return `${lecturerName} - ${monthAndDay} - ${time}`;
+        }
+
+        return false;
+    }
+}());
